23f050081f60c2d377f767a934d8c9f6c1450577
recetapublicitaria <156853694+recetapublicitaria@users.noreply.github.com>
2026-01-13 19:32:57 -0600

    Create texts.js

---
+++ services/texts.js
@@ -0,0 +1,6 @@
+module.exports = {
+  greeting: "Hola 💜 soy Soni, ¿en qué puedo apapacharte hoy?",
+  error:
+    "Ups… tuve un pequeño problema para responderte 😢 ¿Puedes intentar de nuevo?",
+  confirmationAsk: "¿Deseas confirmar esta cita? (sí / no)",
+};
