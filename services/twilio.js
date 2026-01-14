babfd8f9060329b15affd229a8c3f31d036b633c
recetapublicitaria <156853694+recetapublicitaria@users.noreply.github.com>
2026-01-13 20:49:10 -0600

    Update twilio.js

--- services/twilio.js
+++ services/twilio.js
@@ -5,16 +5,14 @@ const client = Twilio(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-async function sendWhats(to, body) {
-  try {
-    await client.messages.create({
-      from: process.env.TWILIO_WHATSAPP_NUMBER,
-      to,
-      body,
-    });
-  } catch (err) {
-    console.error("❌ Error enviando mensaje WhatsApp:", err);
-  }
+async function sendMessage(to, body) {
+  return client.messages.create({
+    from: process.env.TWILIO_WHATSAPP_NUMBER,
+    to,
+    body,
+  });
 }
 
-module.exports = { sendWhats };
+module.exports = {
+  sendMessage,
+};
