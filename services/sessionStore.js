372a3e48f7612b1628ddb32a132f2c4293cb86b3
recetapublicitaria <156853694+recetapublicitaria@users.noreply.github.com>
2026-01-13 20:49:39 -0600

    Update sessionStore.js

--- services/sessionStore.js
+++ services/sessionStore.js
@@ -1,13 +1,5 @@
-// SessionStore — interfaz estándar del motor Sofia
-
-const {
-  getSession,
-  updateSession,
-  resetSession,
-} = require("./session");
-
 module.exports = {
-  get: (id) => getSession(id),
-  set: (id, data) => updateSession(id, data),
-  reset: (id) => resetSession(id),
+  get: getSession,
+  set: updateSession,
+  reset: resetSession,
 };
